bcd598f452c10bf5a75e7944de502dbb92f44d6c
madeleinehuish <vasistha@hotmail.com>
2016-10-04 11:09:13 -0700

    add GET /tracks middleware

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,7 +1,21 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
+const { camelizeKeys } = require('humps');
 
 const router = express.Router();
+
+router.get('/tracks', (_req, res, next) => {
+  knex('tracks')
+    .orderBy('title')
+    .then((rows) => {
+      const tracks = camelizeKeys(rows);
+      res.send(tracks);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
